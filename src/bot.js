3c4c63c5f184bd9ef2480e4240fa893f6caa1f84
MPH <misha.medvedev2001@gmail.com>
2020-03-02 20:19:05 +0200

    Added pas'halochka

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = process.env.TOKEN
+let firstPrihod = true;
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
@@ -16,26 +17,31 @@ const shuffle = (a) => {
     return a;
 }
 
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//     let fromID = msg.from.id;
-//     let resp = match[1]
-//     bot.sendMessage(fromID, resp)
-// })
+bot.onText(/\/hat/,async(msg)=>{
+    await bot.sendAnimation(msg.chat.id,'./ezgif-3-8778d6cac75c.gif')
+    await bot.sendMessage(msg.chat.id, "Bezmenov style 😎😎😎")
+})
 
 const isAdmin = (username) => adminUsernames.findIndex(name => username === name) !== -1
+
+bot.onText(/\/help/,(msg)=>{
+    bot.sendMessage(msg.chat.id, '/startQueue@mph_test1_bot - starts (and restarts) the queue (only for adminkas)\n' +
+     '/addme@mph_test1_bot - pushs your butt into the queue :)' + 
+     '/getList@mph_test1_bot - returns the shuffled list first, then without shuffling (only for adminkas)')
+})
 
 bot.onText(/\/startQueue/, (msg) => {
     if (!isAdmin(msg.from.username)) 
        return bot.sendMessage(msg.chat.id, "Don\'t touch this shit, you\'re not adminka!")
     
     queue = []
-    bot.sendMessage(msg.chat.id, "Queue started")
+    bot.sendMessage(msg.chat.id, "The queue was started))))")
     
 })
 
 bot.onText(/\/addme/, (msg) => {
     if(queue === undefined)
-      return bot.sendMessage(msg.chat.id, "Please start the queue")
+      return bot.sendMessage(msg.chat.id, "Please start the queue)")
     const userName = msg.from.first_name + " " + msg.from.last_name
     const userId = msg.from.id
 
@@ -44,37 +50,42 @@ bot.onText(/\/addme/, (msg) => {
         userId
     }
 
-
     const index = queue.findIndex(element => element.userId === user.userId)
     if (index !== -1)
-        return bot.sendMessage(msg.chat.id, "Noup")
+        return bot.sendMessage(msg.chat.id, "Galayko, perelogyns\'ya, you\'re already in the queue!!!")
 
     queue.push({
         userName,
         userId
     })
-    bot.sendMessage(msg.chat.id, "Added")
+    bot.sendMessage(msg.chat.id, "You\'ve been succesfully to the queue :)")
 })
 
-bot.onText(/\/getlist/, (msg) => {
+bot.onText(/\/getList/, (msg) => {
 
     if(queue === undefined)
-      return bot.sendMessage(msg.chat.id, "Please start the queue")
+      return bot.sendMessage(msg.chat.id, "Please start the queue)")
 
     if(!isAdmin(msg.from.username))
-       return bot.sendMessage(msg.chat.id, "Don\'t touch this shit, you\'re not adminka!")
+       return bot.sendMessage(msg.chat.id, "Don\'t touch this shit, you\'re not adminka!!!")
+
+    if(queue.length === 0)
+      return bot.sendMessage(msg.chat.id, "There\'s no people in the list, add someone first)")
     
-    let listString = ''
+    if(firstPrihod){
+       queue = shuffle(queue)
+       firstPrihod = false
+       bot.sendMessage(msg.chat.id, "I shuffled the list))), here it is:")
+    }
 
-    queue = shuffle(queue)
-    queue.forEach((pidr, index) => {
-        listString += (index + 1) + '. ' + pidr.userName + ';\n'
-    })
-
-    queue = undefined;
-
-    if (listString === '')
-        return bot.sendMessage(msg.chat.id, "No people in the list")
-    
-    bot.sendMessage(msg.chat.id, listString)
+    const stringList = getListForMessage()
+    bot.sendMessage(msg.chat.id, stringList)
 })
+
+const getListForMessage = ()=>{
+    let listString = 'THE LIST:\n'
+    queue.forEach((pidr, index) => {
+        listString += (index + 1) + '. ' + ((index === 0) ? '⭐️' : '') + pidr.userName + ((index === 0) ? '⭐️' : '') + ';\n'
+    })
+    return listString;
+}
